a40feeeb0baa54905cde237c6d3fc5b83fd97b4c
ollisam <142885945+ollisam@users.noreply.github.com>
2024-09-09 18:03:47 +0000

    bonus code

--- code_skeleton/design_principles/1.3.3_bonus_code_skeleton.py
+++ code_skeleton/design_principles/1.3.3_bonus_code_skeleton.py
@@ -1,101 +1,144 @@
-from dataclasses import dataclass
-from enum import Enum
-from typing import List
+# from dataclasses import dataclass
+# from enum import Enum
+# from typing import List
 
-class ShipmentType(Enum):
-    plane = 0
-    ship = 1
-    car = 2
-    carrier_pigeon = 3
-
-
-@dataclass
-class Size:
-    width: float
-    height: float
-    weight: float
+# class ShipmentType(Enum):
+#     plane = 0
+#     ship = 1
+#     car = 2
+#     carrier_pigeon = 3
 
 
-@dataclass
-class Product:
-    name: str
-    price: float
-    size: Size
+# @dataclass
+# class Size:
+#     width: float
+#     height: float
+#     weight: float
 
 
-@dataclass
-class Address:
-    country: str
-    street_address: str
-    city: str
-    zip_code: int
+# @dataclass
+# class Product:
+#     name: str
+#     price: float
+#     size: Size
 
 
-@dataclass
-class Person:
-    name: str
-    ssn: str
-    address: Address
+# @dataclass
+# class Address:
+#     country: str
+#     street_address: str
+#     city: str
+#     zip_code: int
 
 
-@dataclass
-class Package:
-    shipment_type: ShipmentType
-    product: Product
-    merchant: Person
-    buyer: Person
+# @dataclass
+# class Person:
+#     name: str
+#     ssn: str
+#     address: Address
 
 
-class Ship:
+# @dataclass
+# class Package:
+#     shipment_type: ShipmentType
+#     product: Product
+#     merchant: Person
+#     buyer: Person
+
+
+# class Ship:
+#     def reload_fuel_oil(self):
+#         print('reloading fuel supplies on ship')
+
+#     def ship(self, package: Package):
+#         print(f'shipping package via ship from {package.merchant.address} to {package.buyer.address}')
+
+
+# class Plane:
+#     def reload_jet_fuel(self):
+#         print('jet fuels can\'t melt steel beams')
+
+#     def fly(self, package: Package):
+#         print(f'flying package from {package.merchant.address} to {package.buyer.address}')
+
+
+# class Car:
+#     def reload_gas(self):
+#         print('adding gasoline... or diesel')
+
+#     def drive(self, package: Package):
+#         print(f'driving package from {package.merchant.address} to {package.buyer.address}')
+
+
+# class CarrierPigeon:
+#     def eat_seeds(self):
+#         print(f'beep beep I\'m a bird')
+
+#     def fly(self, package: Package):
+#         print(f'beep beep I\'m a bird')
+
+
+# class ShipmentService:
+#     def ship(self, package: Package):
+#         if package.shipment_type == ShipmentType.ship:
+#             ship = Ship()
+#             ship.reload_fuel_oil()
+#             ship.ship(package)
+
+#         elif package.shipment_type == ShipmentType.plane:
+#             plane = Plane()
+#             plane.reload_jet_fuel()
+#             plane.fly(package)
+
+#         elif package.shipment_type == ShipmentType.car:
+#             car = Car()
+#             car.reload_gas()
+#             car.drive(package)
+
+#         elif package.shipment_type == ShipmentType.carrier_pigeon:
+#             pigeon = CarrierPigeon()
+#             pigeon.eat_seeds()
+#             pigeon.fly(package)
+
+from abc import ABC, abstractmethod
+
+class Shipment(ABC):
+    @abstractmethod
+    def ship(self, package):
+        pass
+
+class Ship(Shipment):
+    def ship(self, package):
+        self.reload_fuel_oil()
+        print(f"Shipping package by ship: {package}")
+
     def reload_fuel_oil(self):
-        print('reloading fuel supplies on ship')
+        print("Reloading fuel oil")
 
-    def ship(self, package: Package):
-        print(f'shipping package via ship from {package.merchant.address} to {package.buyer.address}')
+class Plane(Shipment):
+    def ship(self, package):
+        self.reload_jet_fuel()
+        print(f"Shipping package by plane: {package}")
 
-
-class Plane:
     def reload_jet_fuel(self):
-        print('jet fuels can\'t melt steel beams')
+        print("Reloading jet fuel")
 
-    def fly(self, package: Package):
-        print(f'flying package from {package.merchant.address} to {package.buyer.address}')
+class Car(Shipment):
+    def ship(self, package):
+        self.reload_gas()
+        print(f"Shipping package by car: {package}")
 
-
-class Car:
     def reload_gas(self):
-        print('adding gasoline... or diesel')
+        print("Reloading gas")
 
-    def drive(self, package: Package):
-        print(f'driving package from {package.merchant.address} to {package.buyer.address}')
+class CarrierPigeon(Shipment):
+    def ship(self, package):
+        self.eat_seeds()
+        print(f"Shipping package by carrier pigeon: {package}")
 
-
-class CarrierPigeon:
     def eat_seeds(self):
-        print(f'beep beep I\'m a bird')
-
-    def fly(self, package: Package):
-        print(f'beep beep I\'m a bird')
-
+        print("Eating seeds")
 
 class ShipmentService:
-    def ship(self, package: Package):
-        if package.shipment_type == ShipmentType.ship:
-            ship = Ship()
-            ship.reload_fuel_oil()
-            ship.ship(package)
-
-        elif package.shipment_type == ShipmentType.plane:
-            plane = Plane()
-            plane.reload_jet_fuel()
-            plane.fly(package)
-
-        elif package.shipment_type == ShipmentType.car:
-            car = Car()
-            car.reload_gas()
-            car.drive(package)
-
-        elif package.shipment_type == ShipmentType.carrier_pigeon:
-            pigeon = CarrierPigeon()
-            pigeon.eat_seeds()
-            pigeon.fly(package)
+    def ship(self, shipment: Shipment, package):
+        shipment.ship(package)
